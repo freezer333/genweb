18a898fde8ecac68961aae70174652c76b39d407
Scott Frees <scott.frees@gmail.com>
2018-08-18 10:35:22 -0400

    check

--- index.js
+++ index.js
@@ -9,24 +9,21 @@ var shell = require('shelljs');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-var root = "/Users/sfrees/projects/ramapo/";;//process.cwd();
-var source_path = "/Users/sfrees/projects/ramapo/source/";//path.join(root, "/source/");
-var site_path = "/Users/sfrees/projects/ramapo/site/";//path.join(root, "/site/");
+var root = "/home/sfrees/projects/ramapo/";;//process.cwd();
+var source_path = "/home/sfrees/projects/ramapo/source/";//path.join(root, "/source/");
+var site_path = "/home/sfrees/projects/ramapo/site/";//path.join(root, "/site/");
 
-function fileExists(filePath)
-{
-    try
-    {
-        return fs.statSync(filePath).isFile();
-    }
-    catch (err)
-    {
-        return false;
-    }
+function fileExists(filePath) {
+  try {
+    return fs.statSync(filePath).isFile();
+  }
+  catch (err) {
+    return false;
+  }
 }
 
-var check_pass = function() {
-  if ( !fileExists(".ftppass")) {
+var check_pass = function () {
+  if (!fileExists(".ftppass")) {
     console.log([
       "You need a .ftppass file at the root of this project.",
       "It should contain the following: ",
@@ -39,7 +36,7 @@ var check_pass = function() {
   }
 }
 
-var deploy = function() {
+var deploy = function () {
 
   check_pass();
   var touch = require("touch");
@@ -58,7 +55,7 @@ var deploy = function() {
   grunt.tasks(['sftp-deploy']);
 }
 
-var build = function() {
+var build = function () {
   marked.setOptions({
     renderer: new marked.Renderer(),
     gfm: true,
@@ -86,19 +83,19 @@ var build = function() {
   var html_close = "</body></html>";
   var head = fs.readFileSync(path.join(root, '/mix/head.html'), "utf-8")
 
-  function build(f){
+  function build(f) {
     var markdown = fs.readFileSync(f, "utf-8");
     var html = marked(markdown);
 
     var rel_to_source_root = path.relative(path.dirname(f), source_path);
-    if (rel_to_source_root.length > 0 ) {
-      rel_to_source_root+= "/";
+    if (rel_to_source_root.length > 0) {
+      rel_to_source_root += "/";
     }
-    var local_head = head.replace("[@root]", function() { return rel_to_source_root});
+    var local_head = head.replace("[@root]", function () { return rel_to_source_root });
 
     var page = html_open + local_head + html + html_close;
 
-    var filename = path.join(path.dirname(f), path.basename(f, ".md")+".html");
+    var filename = path.join(path.dirname(f), path.basename(f, ".md") + ".html");
     var file_base = path.relative(source_path, filename);
     var out_filename = path.join(site_path, file_base);
     var out_dir = path.dirname(out_filename);
@@ -106,48 +103,48 @@ var build = function() {
 
 
     mkdirp(out_dir, function (err) {
-          if (err) console.error(err)
-          else {
-            fs.writeFile(out_filename, page, {}, function(err) {
-              if (err) throw err;
-              console.log("Built " + out_filename);
-            })
-          }
-      });
+      if (err) console.error(err)
+      else {
+        fs.writeFile(out_filename, page, {}, function (err) {
+          if (err) throw err;
+          console.log("Built " + out_filename);
+        })
+      }
+    });
   }
 
 
-  dir.files(source_path, function(err, files) {
-      if (err) throw err;
-      files.forEach(function (f) {
-        var copy = ig.filter([f]).length > 0;
-        if (path.extname(f) == '.md') {
-          build(f);
-        }
-        else if (copy) {
-          console.log("Copying " + f);
-          var file_base = path.relative(source_path, f);
-          var out_filename = path.join(site_path, file_base);
-          var out_dir = path.dirname(out_filename);
-          mkdirp(out_dir, function (err) {
-            shell.cp(f, out_filename);
-          });
-        }
-        else {
-          console.log("Ignoring " + f);
-        }
-      });
+  dir.files(source_path, function (err, files) {
+    if (err) throw err;
+    files.forEach(function (f) {
+      var copy = ig.filter([f]).length > 0;
+      if (path.extname(f) == '.md') {
+        build(f);
+      }
+      else if (copy) {
+        console.log("Copying " + f);
+        var file_base = path.relative(source_path, f);
+        var out_filename = path.join(site_path, file_base);
+        var out_dir = path.dirname(out_filename);
+        mkdirp(out_dir, function (err) {
+          shell.cp(f, out_filename);
+        });
+      }
+      else {
+        console.log("Ignoring " + f);
+      }
+    });
   });
 
 }
 
-if ( argv._.length == 0 ) {
+if (argv._.length == 0) {
   console.log("You need to either build or deploy!");
 }
 else if (argv._[0] == 'deploy') {
   deploy();
 }
-else if (argv._[0] == 'build'){
+else if (argv._[0] == 'build') {
   build();
   console.log("Run locally - npm install http-server -g");
 }
